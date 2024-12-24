57a450069e4cd5e729845c07bb92021b19181eab
hukuryo <ryohei.1998.05.09@gmail.com>
2024-12-25 08:26:43 +0900

    add package

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="flex gap-3">
-        <Button isLoading color="primary">
-          Loading
+        <Button isLoading={true} color="primary">
+          登録
         </Button>
       </div>
     </section>
