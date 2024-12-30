d88886402b3db2a4fa951b86a50f6f6ba68f6c52
hukuryo <ryohei.1998.05.09@gmail.com>
2024-12-30 20:48:49 +0900

    add tabs component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import DateInputForm from "@/components/datePicker";
 import { NuqsForm } from "@/components/nuqsForm";
+import TabsComp from "@/components/tabs";
 
 export default function Home() {
   return (
     <>
-      <NuqsForm />
+      <TabsComp />
     </>
   );
 }
